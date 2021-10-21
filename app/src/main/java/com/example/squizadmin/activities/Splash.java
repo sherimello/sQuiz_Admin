20a3baa482c458c48a88388cb56ab5d271c6c073
sherimello <s.r.1769karega@gmail.com>
2021-10-21 22:32:24 +0600

    Initial commit

---
+++ app/src/main/java/com/example/squizadmin/activities/Splash.java
@@ -0,0 +1,16 @@
+package com.example.squizadmin.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.squizadmin.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
